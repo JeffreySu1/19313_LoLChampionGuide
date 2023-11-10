763864d416dcb14dd42c6490d27adad3d4b23478
JeffreySu1 <105891307+JeffreySu1@users.noreply.github.com>
2023-11-10 09:37:51 +0000

    Added a trim for the comment input if it was empty

--- annie.js
+++ annie.js
@@ -1,13 +1,17 @@
+// Updates and displays comments as well as average rating from the API
+
+// Displays comments from API
 $(document).ready(function () {
   function displayComments() {
     $.ajax({
-      url: "https://retoolapi.dev/COcivp/data",
+      url: "https://retoolapi.dev/GybHFL/data",
       method: "GET",
       success: function (response) {
         const commentsContainer = $("#comments-container");
         const commentCountElement = $("#comment-count");
         commentsContainer.empty();
 
+        // Checks if anything has been typed into the box
         if (Array.isArray(response) && response.length > 0) {
           response.forEach(function (comment) {
             if (comment.comment) {
@@ -15,6 +19,7 @@ $(document).ready(function () {
             }
           });
 
+          // Displays all the comments in the API
           const totalComments = response.length - 1;
           commentCountElement.text("Total Comments: " + totalComments);
         } else {
@@ -28,6 +33,7 @@ $(document).ready(function () {
     });
   }
 
+  // Displays an average rating from the API
   function displayRatings() {
     $.ajax({
       url: "https://retoolapi.dev/vWUBoA/data",
@@ -37,6 +43,7 @@ $(document).ready(function () {
         const ratingCountElement = $("#rating-count");
         ratingsContainer.empty();
 
+        // Checks if anything has been typed into the box
         if (Array.isArray(response) && response.length > 0) {
           let totalRatings = 0;
           response.forEach(function (rating) {
@@ -46,6 +53,7 @@ $(document).ready(function () {
             }
           });
 
+          // Calculates and displays the average rating
           const averageRating = totalRatings / response.length;
           ratingCountElement.text("Average Rating: " + averageRating.toFixed(1));
         } else {
@@ -59,18 +67,25 @@ $(document).ready(function () {
     });
   }
 
+  // Displays the current comments and average rating when loading page
   displayComments();
   displayRatings();
 
+  // Adds new comment to the API
   $("#submitCommentBtn").click(function () {
     const userInput = $("#userInput").val();
 
+  // Checks if anything has been typed into the box
+  if (userInput.trim() !== "") {
+    
+    // POST new comment to API
     $.ajax({
-      url: "https://retoolapi.dev/COcivp/data",
+      url: "https://retoolapi.dev/GybHFL/data",
       method: "POST",
       data: JSON.stringify({ comment: userInput }),
       contentType: "application/json",
       success: function () {
+        // Updates the comments displayed and clears comment box
         displayComments();
         $("#userInput").val("");
       },
@@ -78,20 +93,24 @@ $(document).ready(function () {
         console.error("API Error:", error);
       },
     });
+  }
   });
 
+  // Adds new rating to the API
   $("#submitRatingBtn").click(function () {
     const ratingInput = $("#ratingInput").val();
 
     //Checks if number is between 1-5
     if (isNumericBetween(ratingInput, 1, 5)) {
 
+      // POST the new rating to API
       $.ajax({
         url: "https://retoolapi.dev/vWUBoA/data",
         method: "POST",
         data: JSON.stringify({ rating: ratingInput }),
         contentType: "application/json",
         success: function () {
+          // Updates the ratings displayed and clears rating box
           displayRatings();
           $("#ratingInput").val("");
         },
@@ -99,6 +118,7 @@ $(document).ready(function () {
           console.error("API Error:", error);
         },
       });
+      // Displays an alert if number isn't between 1-5
     } else {
       alert("Please enter a number between 1 and 5.")
     }
